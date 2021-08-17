a39ccf2169b4ea3d22bae50395bbf328077ae15c
Christine Contreras <christineec23@yahoo.com>
2021-08-17 12:02:05 -0500

    working

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.chars.last == "." ? true : false
   end
 
   def question?
-
+    self.chars.last == "?" ? true : false
   end
 
   def exclamation?
-
+    self.chars.last == "!" ? true : false
   end
 
   def count_sentences
-
+    self.split(/[\!\.\?]/).filter { |sentence| !sentence.empty? }.length
   end
 end
